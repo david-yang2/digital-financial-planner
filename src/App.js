3d19881b8f619329f0ea1a642fde5982f4694a3b
David <david.yang026@gmail.com>
2023-10-15 09:39:48 -0700

    removed boilerplate

--- src/App.js
+++ src/App.js
@@ -6,22 +6,8 @@ import Automobile from './components/automobile/automobile.jsx'
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
         <Mortgage />
         <Automobile />
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
