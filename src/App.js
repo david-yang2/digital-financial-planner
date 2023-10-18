fbb30c65aa3fe7a4c18956df3a00e643ba43514c
David <david.yang026@gmail.com>
2023-10-18 10:09:38 -0700

    removed automobile import

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
 import Mortgage from './components/mortgage/mortgage.jsx'
-import Automobile from './components/automobile/automobile.jsx'
 import Income from "./components/income/income.jsx"
 
 function App() {
