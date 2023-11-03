6226e236f45641b3b00892c27451123d15ad7e60
David <david.yang026@gmail.com>
2023-11-02 22:03:49 -0700

    mortgage/automobile buttons - conditional rendering the components

--- src/components/calculator/calculator.jsx
+++ src/components/calculator/calculator.jsx
@@ -1,12 +1,29 @@
 import Mortgage from "../mortgage/mortgage.jsx"
 import Automobile from "../automobile/automobile.jsx"
+import {useState} from "react";
+
 
 const Calculator = () => {
+    const [showMortgage, setShowMortgage] = useState(false);
+    const [showAutomobile, setShowAutomobile] = useState(false);
+
+    const toggleMortgageComponent = () => {
+        setShowMortgage(!showMortgage);
+        setShowAutomobile(false);
+    }
+
+    const toggleAutomobileComponent = () => {
+        setShowAutomobile(!showAutomobile);
+        setShowMortgage(false);
+    }
+
     return (
         <div>
             <h1>this is the calculator component</h1>
-            <Mortgage />
-            <Automobile />
+            <button onClick={toggleMortgageComponent}> Mortgage </button>
+            <button onClick={toggleAutomobileComponent}> Automobile </button>
+            {showMortgage && <Mortgage />}
+            {showAutomobile && <Automobile />}
         </div>
     )
 }
